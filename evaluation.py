15d26a2017e233180e6d39ff2e0f1f15ce1e6441
Elahe Sdn <elasdn273@gmail.com>
2026-02-21 23:41:29 +0330

    adding acc

--- evaluation.py
+++ evaluation.py
@@ -2,12 +2,13 @@ import torch
 
 def evaluate(model, loader, device):
     """
-    Evaluates the model on a dataset and computes Top-1
-    and Top-5 accuracy.
+    Evaluates the model and computes:
+    Top-1, Top-5, Top-10 and Accuracy
     """
     model.eval()
     top1 = 0
     top5 = 0
+    top10 = 0
     total = 0
 
     with torch.no_grad():
@@ -15,14 +16,24 @@ def evaluate(model, loader, device):
             x, y = x.to(device), y.to(device)
             out = model(x)
 
-            _, pred = out.topk(5, dim=1)
+            # گرفتن 10 تای اول
+            _, pred = out.topk(10, dim=1)
 
-            top1 += (pred[:,0] == y).sum().item()
+            # Top-1
+            top1 += (pred[:, 0] == y).sum().item()
 
-            for i in range(y.size(0)):
-                if y[i] in pred[i]:
-                    top5 += 1
+            # Top-5 
+            top5 += (pred[:, :5] == y.unsqueeze(1)).any(dim=1).sum().item()
+
+            # Top-10
+            top10 += (pred == y.unsqueeze(1)).any(dim=1).sum().item()
 
             total += y.size(0)
 
-    return top1/total, top5/total
+    top1_acc = top1 / total
+    top5_acc = top5 / total
+    top10_acc = top10 / total
+
+    accuracy = top1_acc 
+
+    return accuracy, top5_acc, top10_acc
